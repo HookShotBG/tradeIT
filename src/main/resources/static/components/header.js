d6e3ae931685a37ab7554d2a827ca5038e574f22
brunrya1@students.zhaw.ch <brunrya1@students.zhaw.ch>
2020-06-01 14:53:06 +0200

    entire header updated with two components  -header  -searchbar

--- src/main/resources/static/components/header.js
+++ src/main/resources/static/components/header.js
@@ -19,13 +19,5 @@ Vue.component('headerview', {
         '          </div>\n' +
         '        </div>\n' +
         '      </div>\n' +
-        '      <div class="row">\n' +
-        '        <div class="col-sm-12">\n' +
-        '          <div class="well">\n' +
-        '            <div class="active-cyan-4 mb-4">\n' +
-        '                <input class="form-control" type="text" placeholder="Search" aria-label="Search">\n' +
-        '              </div>\n' +
-        '              </div>\n' +
-        '        </div>\n' +
-        '      </div></div>'
+        '      </div>'
 })
---
+++ src/main/resources/static/components/searchbar.js
@@ -0,0 +1,11 @@
+Vue.component('searchbar', {
+    template: '<div class="row">\n' +
+        '          <div class="col-sm-12">\n' +
+        '            <div class="well">\n' +
+        '              <div class="active-cyan-4 mb-4">\n' +
+        '                <input class="form-control" type="text" placeholder="Search" aria-label="Search">\n' +
+        '              </div>\n' +
+        '            </div>\n' +
+        '          </div>\n' +
+        '        </div>'
+})
