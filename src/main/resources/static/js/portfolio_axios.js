1085a981e4250cb667c179adb5c4805f8a61359a
daniellutziger <daniel.lutziger@hispeed.ch>
2020-06-03 00:55:00 +0200

    chart generated by axios and stuff.. refreshed every time value gets added

--- src/main/resources/static/js/portfolio_axios.js
+++ src/main/resources/static/js/portfolio_axios.js
@@ -6,11 +6,15 @@ new Vue({
         classes: {
             isLoss: false,
             isProfit: false
-        }
+        },
+        resultSet: null,
+        chart: null
     },
     mounted: function () {
         this.getPortfolioData();
         this.getOverviewData();
+        this.drawChart();
+
     },
     methods: {
         getPortfolioData: function(){
@@ -21,7 +25,6 @@ new Vue({
         getOverviewData: function(){
           axios.get('/findSingleTrade/2').then(response => (this.overview_json = response.data));
         },
-
         calculateProfitLoss: function(currentPreis, investedPreis){
             pl = currentPreis - investedPreis
             pl = Math.round((pl + Number.EPSILON) * 100) / 100
@@ -35,23 +38,7 @@ new Vue({
             }
             return pl
         },
-        calculateProfitLossPerc: function(currentPreis, investedPreis){
-            var pl = currentPreis*100/investedPreis-100;
-            pl = Math.round((pl + Number.EPSILON) * 100) / 100
-            if(pl >= 0){
-                pl = '+' + pl + "%";
-                this.classes.isProfit = false;
-                this.classes.isLoss= true;
-            }else {
-                this.classes.isProfit = true;
-                this.classes.isLoss = false;
-            }
-            return pl
-        },
-
         calculations: function (firstValue, secondValue) {
-            console.log(firstValue);
-            console.log(secondValue);
             var priceChange = firstValue*100/secondValue-100;
             priceChange = Math.round((priceChange + Number.EPSILON) * 100) / 100
             if(priceChange >= 0){
@@ -64,26 +51,12 @@ new Vue({
             }
             return priceChange;
         },
-        createNewPreis : async function (event){
-            const newPreis = Math.floor((Math.random() * 100) + 1);
-            const postRequest = 'http://localhost:8080/preis/' + newPreis;
-            axios.post(postRequest);
-            await axios.get('/preis/latestPreis').then(response => (this.json[this.json.length-1].preis.push(response.data))).resolve(this.calculations(this.json[this.json.length-1].preis[this.json[this.json.length-1].preis.length-1].preis, this.json[this.json.length-1].preis[this.json[this.json.length-1].preis.length-2].preis));
-
-
-
-
-            //this.calculations(this.json[this.json.length-1].preis[this.json[this.json.length-1].preis.length-1].preis, this.json[this.json.length-1].preis[this.json[this.json.length-1].preis.length-2].preis);
-
-        },
-        something : function (){
+        createNewPreis : function (){
             this.addPrice().then((result) =>{
                 this.latestPrice().then((used) => {
-                    console.log("penisman");
-                    console.log(result);
-                    console.log(this.json[this.json.length-1].preis[this.json[this.json.length-1]].preis);
-                    console.log(this.json[this.json.length-1].preis[this.json[this.json.length-2]].preis);
-
+                    this.calculations(this.json[this.json.length-1].preis[this.json[this.json.length-1].preis.length-1].preis, this.json[this.json.length-1].preis[this.json[this.json.length-1].preis.length-2].preis);
+                    this.drawChart();
+                    this.calculateProfitLoss();
                 })
             })
         },
@@ -94,6 +67,73 @@ new Vue({
         },
         latestPrice (){
             return axios.get('/preis/latestPreis').then(response => (this.json[this.json.length-1].preis.push(response.data)));
+        },
+        drawChart(){
+            axios
+                .get('/stockX')
+                .then((response) => {
+                    this.resultSet = response.data;
+
+
+                    this.resultSet[1].preis.sort(compare);
+
+                    function compare(a, b) {
+                        var date1 = a.idPreis;
+                        var date2 = b.idPreis;
+                        var comparison = 0;
+                        if (date1 > date2) {
+                            comparison = 1;
+                        } else if (date1 < date2) {
+                            comparison = -1;
+                        }
+                        return comparison;
+                    }
+
+                    var xf = [];
+                    this.resultSet[1].preis.forEach((value, key) => {
+                        var ff = new Date(value.valid_until);
+                        var t = {
+                            x: ff,
+                            y:value.preis,
+                            indexLabel: "", markerType: "circle",  markerColor: "#6B8E23"
+                        };
+
+                        if(key > 0){
+                            if(value.preis < this.resultSet[1].preis[key-1].preis){
+                                t.indexLabel = "";
+                                t.markerType = "circle";
+                                t.markerColor = "tomato";
+                            }
+                        }
+                        xf.push(t);
+                    })
+                    this.chart = new CanvasJS.Chart("share_chartContainer", {
+                        theme: "light1",
+                        animationEnabled: true,
+                        title:{
+                            text: this.resultSet[1].name
+                        },
+                        axisX: {
+                            interval: 1,
+                            intervalType: "day",
+                            valueFormatString: "DD"
+                        },
+                        axisY:{
+                            title: "Price (in CHF)",
+                            valueFormatString: "$#0"
+                        },
+                        data: [{
+                            type: "line",
+                            markerSize: 12,
+                            xValueFormatString: "MMM, YYYY",
+                            yValueFormatString: "$###.#",
+                            dataPoints: xf
+                        }]
+                    });
+                    this.chart.render();
+
+                });
         }
     }
+
 });
